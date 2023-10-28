07f238697827824f6f9bde0680baae769ae88487
lapinRagnar <lapinragnar@gmail.com>
2023-10-28 18:30:01 +0200

    fin de la navbar pour le mobile est effiché il n'est pas afficher qd l'ecran est grand

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -2,11 +2,13 @@ import { Link, useNavigate } from "react-router-dom"
 import { Button } from "../ui/button"
 import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations"
 import { useEffect } from "react"
+import { useUserContext } from "@/context/AuthContext"
 
 const Topbar = () => {
 
   const { mutate: signOut, isSuccess } = useSignOutAccount()
   const navigate = useNavigate()
+  const { user } = useUserContext()
 
   useEffect(() => {
     if (isSuccess) {
@@ -15,26 +17,36 @@ const Topbar = () => {
   }, [isSuccess, navigate])   // le navigate on peut l'enlever
 
   return (
-    <section className=""> 
+    <section className="topbar"> 
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
           <img 
             src="/assets/images/logo4-1.png"
             alt="logo"
-            width={230}
-            height={325}
+            width={100}
+            height={100}
           />
         </Link>
-      </div>
-      <div className="flex gap-4">
-        <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
-          <img 
-            src="/assets/icons/logout2.svg"
-            alt="logout"
-            width={50}
-            height={50}
-          />
-        </Button>
+      
+        <div className="flex gap-1">
+          <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
+            <img 
+              src="/assets/icons/logout2.svg"
+              alt="logout"
+              width={30}
+              height={30}
+            />
+          </Button>
+
+          <Link to={`/profile/${user?.id}`} className="flex-center gap-3">
+            <img 
+              src={user.imageUrl || '/assets/images/avatar.svg'}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
+            />
+          </Link>
+
+        </div>
       </div>     
     </section>
   )
